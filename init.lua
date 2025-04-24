56ee6e85e93ed662b58560b065f7abb5c8f48095
alfie <alfiebooth@gmail.com>
2025-04-24 07:57:57 +0100

    incremental

--- init.lua
+++ init.lua
@@ -826,10 +826,6 @@ require('lazy').setup({
             end
           end, { desc = 'Smart close buffer without closing window' })
 
-          -- coiplot stuff
-          vim.keymap.set('n', '<leader>ce', ':Copilot enable<CR>', { desc = '[C]opilot [E]nable' })
-          vim.keymap.set('n', '<leader>cd', ':Copilot disable<CR>', { desc = '[C]opilot [D]isable' })
-
           -- leap.nvim stuff
           vim.keymap.set('n', 's', '<Plug>(leap-anywhere)')
           vim.keymap.set('x', 's', '<Plug>(leap)')
@@ -1547,6 +1543,20 @@ require('lazy').setup({
 
   {
     'github/copilot.vim',
+    config = function()
+      -- NOTE: with the below config then we only get copilot suggesting when we hit ctrl and k (i chose this as its both middle fingers), and you accept with tab
+      -- vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+      vim.g.copilot_enabled = false
+
+      vim.keymap.set('i', '<C-k>', '<Plug>(copilot-suggest)', {
+        desc = 'Manually trigger Copilot suggestion',
+      })
+
+      -- these are the keymaps that are set by default
+      vim.keymap.set('n', '<leader>ce', ':Copilot enable<CR>', { desc = '[C]opilot [E]nable' })
+      vim.keymap.set('n', '<leader>cd', ':Copilot disable<CR>', { desc = '[C]opilot [D]isable' })
+    end,
   },
   {
     'akinsho/bufferline.nvim',

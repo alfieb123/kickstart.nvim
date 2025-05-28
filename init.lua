c39f9af42237cfa637f41b06f0dd673dde9c05f6
alfie <alfiebooth@gmail.com>
2025-05-28 13:02:42 +0100

    incremental

--- init.lua
+++ init.lua
@@ -241,6 +241,7 @@ vim.keymap.set('n', '<leader>fa', function()
   vim.cmd 'set foldexpr=nvim_treesitter#foldexpr()'
   vim.cmd 'normal! zM'
 end, { desc = 'Fold all functions' })
+
 vim.keymap.set('n', '<leader>fu', 'zR', { desc = 'Unfold all functions' })
 -- vim.keymap.set('n', '<leader>kl', 'zO', { desc = 'Expand current fold recursively' })
 vim.keymap.set('n', '<leader>ff', function()
@@ -1717,22 +1718,24 @@ require('lazy').setup({
 function HighlightedFoldtext()
   local pos = vim.v.foldstart
   local line = vim.api.nvim_buf_get_lines(0, pos - 1, pos, false)[1]
-  local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
-  local parser = vim.treesitter.get_parser(0, lang)
-  local query = vim.treesitter.query.get(parser:lang(), 'highlights')
 
+  local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
+  local ok, parser = pcall(vim.treesitter.get_parser, 0, lang)
+  if not ok or not parser then
+    return vim.fn.foldtext()
+  end
+
+  local query = vim.treesitter.query.get(parser:lang(), 'highlights')
   if query == nil then
     return vim.fn.foldtext()
   end
 
   local tree = parser:parse({ pos - 1, pos })[1]
   local result = {}
-
   local line_pos = 0
-
   local prev_range = nil
 
-  for id, node, _ in query:iter_captures(tree:root(), 0, pos - 1, pos) do
+  for id, node in query:iter_captures(tree:root(), 0, pos - 1, pos) do
     local name = query.captures[id]
     local start_row, start_col, end_row, end_col = node:range()
     if start_row == pos - 1 and end_row == pos - 1 then
@@ -1742,7 +1745,7 @@ function HighlightedFoldtext()
       end
       line_pos = end_col
       local text = vim.treesitter.get_node_text(node, 0)
-      if prev_range ~= nil and range[1] == prev_range[1] and range[2] == prev_range[2] then
+      if prev_range and range[1] == prev_range[1] and range[2] == prev_range[2] then
         result[#result] = { text, '@' .. name }
       else
         table.insert(result, { text, '@' .. name })
@@ -1753,6 +1756,45 @@ function HighlightedFoldtext()
 
   return result
 end
+-- function HighlightedFoldtext()
+--   local pos = vim.v.foldstart
+--   local line = vim.api.nvim_buf_get_lines(0, pos - 1, pos, false)[1]
+--   local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
+--   local parser = vim.treesitter.get_parser(0, lang)
+--   local query = vim.treesitter.query.get(parser:lang(), 'highlights')
+--
+--   if query == nil then
+--     return vim.fn.foldtext()
+--   end
+--
+--   local tree = parser:parse({ pos - 1, pos })[1]
+--   local result = {}
+--
+--   local line_pos = 0
+--
+--   local prev_range = nil
+--
+--   for id, node, _ in query:iter_captures(tree:root(), 0, pos - 1, pos) do
+--     local name = query.captures[id]
+--     local start_row, start_col, end_row, end_col = node:range()
+--     if start_row == pos - 1 and end_row == pos - 1 then
+--       local range = { start_col, end_col }
+--       if start_col > line_pos then
+--         table.insert(result, { line:sub(line_pos + 1, start_col), 'Folded' })
+--       end
+--       line_pos = end_col
+--       local text = vim.treesitter.get_node_text(node, 0)
+--       if prev_range ~= nil and range[1] == prev_range[1] and range[2] == prev_range[2] then
+--         result[#result] = { text, '@' .. name }
+--       else
+--         table.insert(result, { text, '@' .. name })
+--       end
+--       prev_range = range
+--     end
+--   end
+--
+--   return result
+-- end
 
 local bg = vim.api.nvim_get_hl(0, { name = 'StatusLine' }).bg
 local hl = vim.api.nvim_get_hl(0, { name = 'Folded' })

53333d471dbb6fbcfcbbe878ae3b5d6b22646ef7
alfie <alfiebooth@gmail.com>
2025-06-13 09:54:29 +0100

    incremental

--- init.lua
+++ init.lua
@@ -1543,6 +1543,14 @@ require('lazy').setup({
         },
         twilight = { enabled = true }, -- if you also use Twilight
         gitsigns = { enabled = false },
+        -- this will change the font size on kitty when in zen mode
+        -- to make this work, you need to set the following kitty options:
+        -- - allow_remote_control socket-only
+        -- - listen_on unix:/tmp/kitty
+        kitty = {
+          enabled = true,
+          font = '+4', -- font size increment
+        },
       },
     },
     keys = {
